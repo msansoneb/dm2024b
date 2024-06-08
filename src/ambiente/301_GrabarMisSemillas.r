78ed74ed0e62c33b287ba69552e2e4248271e898
msansoneb <msansone@itba.edu.ar>
2024-06-08 20:11:57 +0000

    auto commit 1       20240608 201155

--- src/ambiente/301_GrabarMisSemillas.r
+++ src/ambiente/301_GrabarMisSemillas.r
@@ -1,15 +1,14 @@
 # Este script almacena definitivamente sus cinco semillas
 # en el bucket, de forma que NO deba cargarlas en cada script
 
-require( "data.table" )
+require("data.table")
 
-# reemplazar aqui por SUS semillas 
+# reemplazar aqui por SUS semillas
 mis_semillas <- c(100019, 100057, 100193, 999931, 999959)
 
-tabla_semillas <- as.data.table(list( semilla = mis_semillas ))
+tabla_semillas <- as.data.table(list(semilla = mis_semillas))
 
-fwrite( tabla_semillas,
-    file = "~/buckets/b1/datasets/mis_semillas.txt",
+fwrite(tabla_semillas,
+    file = "~/buckets/b1/datasets/mis_semillas.txt", # nolint
     sep = "\t"
 )
-
