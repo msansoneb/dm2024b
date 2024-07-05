ab22b7c595cd7d0318d895556b3f6a16528f4ac1
msansoneb <msansone@itba.edu.ar>
2024-07-05 13:47:43 +0000

    auto commit 1       20240705 134741

--- src/workflows/619_workflow_base_f202109.r
+++ src/workflows/619_workflow_base_f202109.r
@@ -179,10 +179,10 @@ FErf_attributes_base <- function( pinputexps, ratio, desvio)
   # parametros para que LightGBM se comporte como Random Forest
   param_local$lgb_param <- list(
     # parametros que se pueden cambiar
-    num_iterations = 20,
-    num_leaves  = 32,
-    min_data_in_leaf = 500,
-    feature_fraction_bynode  = 0.2,
+    num_iterations = 50,
+    num_leaves  = 40,
+    min_data_in_leaf = 300,
+    feature_fraction_bynode  = 0.1,
 
     # para que LightGBM emule Random Forest
     boosting = "rf",
